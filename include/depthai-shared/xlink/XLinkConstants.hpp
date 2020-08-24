5612ea2756768418dd505a4479a392058cc39f80
Martin Peterlin <martin.peterlin7@gmail.com>
2020-08-24 20:36:32 +0200

    RPC channel

--- include/depthai-shared/xlink/XLinkConstants.hpp
+++ include/depthai-shared/xlink/XLinkConstants.hpp
@@ -4,7 +4,10 @@ namespace dai
 {
     // channel names
     constexpr static const char* XLINK_CHANNEL_PIPELINE_CONFIG = "__pipeline_config";
+    constexpr static const char* XLINK_CHANNEL_MAIN_RPC = "__rpc_main";
     
+
+
     // usb buffer maximum size
     constexpr static int XLINK_USB_BUFFER_MAX_SIZE = 5 * 1024 * 1024;
 
