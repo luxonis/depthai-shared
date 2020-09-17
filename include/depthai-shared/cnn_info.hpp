dc262fa6f2731430e9f6d877fbbac9c8f200b0f5
GergelySzabolcs <szabi@luxonis.com>
2020-09-17 17:51:57 +0300

    Add updated tensor representation

--- include/depthai-shared/cnn_info.hpp
+++ include/depthai-shared/cnn_info.hpp
@@ -2,6 +2,26 @@
 
 #include <cstdint>
 
+
+struct detection_t
+{
+    uint32_t label;
+    float confidence;
+    float x_min;
+    float y_min;
+    float x_max;
+    float y_max;
+    float depth_x;
+    float depth_y;
+    float depth_z;
+};
+
+struct detection_out_t
+{
+    int detection_count;
+    detection_t detections[100];
+};
+
 struct nn_to_depth_bbox_map
 {
     uint16_t offset_x;
@@ -10,14 +30,12 @@ struct nn_to_depth_bbox_map
     uint16_t max_height;
 };
 
-struct cnn_info
+
+typedef enum TensorDataType : int32_t
 {
-    uint16_t cnn_input_width;
-    uint16_t cnn_input_height;
-    uint16_t cnn_input_num_channels;
-    uint16_t number_of_cmx_slices;
-    uint16_t number_of_shaves;
-    uint32_t offsets[7];
-    nn_to_depth_bbox_map nn_to_depth;
-    int32_t satisfied_resources;
-};
+    _fp16 = 0,                  ///< Half precision floating point
+    _u8f  = 1,                  ///< Unsigned byte
+    _int  = 2,                  ///< Signed integer (4 byte)
+    _fp32 = 3,                  ///< Single precision floating point
+    _i8   = 4,                  ///< Signed byte
+} TensorDataType;
