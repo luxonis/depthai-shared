fda9b90bfdd37afc33b23ba337ccecf95d2d81a0
Jon Ngai <jon.ngai@gmail.com>
2020-10-02 17:40:13 -0600

    Adding placeholder for SPIOutProperties.hpp.

---
+++ include/depthai-shared/pb/properties/SPIOutProperties.hpp
@@ -0,0 +1,28 @@
+//  To parse this JSON data, first install
+//
+//      Boost     http://www.boost.org
+//      json.hpp  https://github.com/nlohmann/json
+//
+//  Then include this file, and then do
+//
+//     SPIOutProperties.hpp data = nlohmann::json::parse(jsonString);
+
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+namespace dai {
+
+    /**
+     * Properties for XLinkOut which define stream name
+     */
+    struct SPIOutProperties {
+        /**
+         * Set a limit to how many packets will be sent further to host
+         */
+        std::string streamName;
+    };
+
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(SPIOutProperties, streamName)
+
+}
