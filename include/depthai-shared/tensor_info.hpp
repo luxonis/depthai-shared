2d83ada9d95ed0ddb331e87fbc2d13bc88a761ed
SzabolcsGergely <szabi@luxonis.com>
2020-10-05 16:35:24 +0300

    Remove redundant tensor_ prefix in TensorInfo fields

--- include/depthai-shared/tensor_info.hpp
+++ include/depthai-shared/tensor_info.hpp
@@ -13,7 +13,7 @@ struct TensorInfo
     public:
     using ShapeType = uint32_t;
 
-    enum class TensorDataType : int32_t
+    enum class DataType : int32_t
     {
         _fp16 = 0,                  ///< Half precision floating point
         _u8f  = 1,                  ///< Unsigned byte
@@ -23,8 +23,8 @@ struct TensorInfo
     };
 
 
-    static const std::map<TensorDataType, unsigned int> c_type_size;
-    static const std::map<TensorDataType, std::string> type_to_string;
+    static const std::map<DataType, unsigned int> c_type_size;
+    static const std::map<DataType, std::string> type_to_string;
 
     enum class Dimension : int32_t
     {   
@@ -49,29 +49,29 @@ struct TensorInfo
         deserialize(tensor_info);
     }
 
-    std::string tensor_name;
+    std::string name;
 
     std::vector<Dimension> order;
-    std::vector<ShapeType> tensor_dimensions;
-    std::vector<ShapeType> tensor_strides;
+    std::vector<ShapeType> dimensions;
+    std::vector<ShapeType> strides;
 
-    TensorDataType tensor_data_type;
-    size_t tensor_offset;
-    size_t tensor_element_size;
-    size_t tensor_idx;
+    DataType data_type;
+    size_t offset;
+    size_t element_size;
+    size_t index;
 
     nlohmann::json serialize()
     {
         nlohmann::json tensor_info;
-        DEPTHAI_SHARED_SET_JSON(tensor_name,           tensor_info["name"]);
-        DEPTHAI_SHARED_SET_JSON(tensor_offset,         tensor_info["offset"]);
-        DEPTHAI_SHARED_SET_JSON(tensor_idx,            tensor_info["idx"]);
+        DEPTHAI_SHARED_SET_JSON(name,           tensor_info["name"]);
+        DEPTHAI_SHARED_SET_JSON(offset,         tensor_info["offset"]);
+        DEPTHAI_SHARED_SET_JSON(index,          tensor_info["idx"]);
 
-        DEPTHAI_SHARED_SET_JSON(tensor_data_type,      tensor_info["shape"]["data_type"]);
-        DEPTHAI_SHARED_SET_JSON(tensor_element_size,   tensor_info["shape"]["tensor_element_size"]);
-        DEPTHAI_SHARED_SET_JSON(order,                 tensor_info["shape"]["order"]);
-        DEPTHAI_SHARED_SET_JSON(tensor_dimensions,     tensor_info["shape"]["dimensions"]);
-        DEPTHAI_SHARED_SET_JSON(tensor_strides,        tensor_info["shape"]["strides"]);
+        DEPTHAI_SHARED_SET_JSON(data_type,      tensor_info["shape"]["data_type"]);
+        DEPTHAI_SHARED_SET_JSON(element_size,   tensor_info["shape"]["element_size"]);
+        DEPTHAI_SHARED_SET_JSON(order,          tensor_info["shape"]["order"]);
+        DEPTHAI_SHARED_SET_JSON(dimensions,     tensor_info["shape"]["dimensions"]);
+        DEPTHAI_SHARED_SET_JSON(strides,        tensor_info["shape"]["strides"]);
         return tensor_info;
     }
 
@@ -83,23 +83,23 @@ struct TensorInfo
             if(order[i] == dim) break;
         }
         if(i == order.size()) assert(0);
-        return tensor_dimensions[i];
+        return dimensions[i];
     }
 
     private:
     void deserialize(nlohmann::json tensor_info)
     {
-        DEPTHAI_SHARED_GET_JSON(tensor_name,       tensor_info["name"]);
-        DEPTHAI_SHARED_GET_JSON(tensor_offset,     tensor_info["offset"]);
-        DEPTHAI_SHARED_GET_JSON(tensor_idx,        tensor_info["idx"]);
+        DEPTHAI_SHARED_GET_JSON(name,       tensor_info["name"]);
+        DEPTHAI_SHARED_GET_JSON(offset,     tensor_info["offset"]);
+        DEPTHAI_SHARED_GET_JSON(index,      tensor_info["idx"]);
 
-        DEPTHAI_SHARED_GET_JSON(tensor_data_type,  tensor_info["shape"]["data_type"]);
-        tensor_element_size = c_type_size.at(tensor_data_type);
-        // DEPTHAI_SHARED_GET_JSON(tensor_element_size,  tensor_info["shape"]["tensor_element_size"]);
+        DEPTHAI_SHARED_GET_JSON(data_type,  tensor_info["shape"]["data_type"]);
+        element_size = c_type_size.at(data_type);
+        // DEPTHAI_SHARED_GET_JSON(element_size,  tensor_info["shape"]["element_size"]);
 
-        DEPTHAI_SHARED_GET_JSON(order,             tensor_info["shape"]["order"]);
-        DEPTHAI_SHARED_GET_JSON(tensor_dimensions, tensor_info["shape"]["dimensions"]);
-        DEPTHAI_SHARED_GET_JSON(tensor_strides,    tensor_info["shape"]["strides"]);
+        DEPTHAI_SHARED_GET_JSON(order,      tensor_info["shape"]["order"]);
+        DEPTHAI_SHARED_GET_JSON(dimensions, tensor_info["shape"]["dimensions"]);
+        DEPTHAI_SHARED_GET_JSON(strides,    tensor_info["shape"]["strides"]);
     }
 };
 }
@@ -108,17 +108,17 @@ struct TensorInfo
 inline std::ostream &operator<<(std::ostream &os, dai::TensorInfo const &t_info)
 {
 
-    os << "Name: " << t_info.tensor_name << std::endl;
-    os << "Index: " << t_info.tensor_idx << std::endl;
-    os << "Element type: " << dai::TensorInfo::type_to_string.at(t_info.tensor_data_type) << std::endl;
-    os << "Element size: " << " " << t_info.tensor_element_size << ((t_info.tensor_element_size == 1) ? "byte" : " bytes") << std::endl;
-    os << "Offset: " << t_info.tensor_offset << " " << ((t_info.tensor_offset <= 1) ? "byte" : " bytes") << std::endl;
+    os << "Name: " << t_info.name << std::endl;
+    os << "Index: " << t_info.index << std::endl;
+    os << "Element type: " << dai::TensorInfo::type_to_string.at(t_info.data_type) << std::endl;
+    os << "Element size: " << " " << t_info.element_size << ((t_info.element_size == 1) ? "byte" : " bytes") << std::endl;
+    os << "Offset: " << t_info.offset << " " << ((t_info.offset <= 1) ? "byte" : " bytes") << std::endl;
     os << "Dimensions: ";
     os << "[";
-    for (unsigned int idx = 0; idx < t_info.tensor_dimensions.size(); idx++)
+    for (unsigned int idx = 0; idx < t_info.dimensions.size(); idx++)
     {
-        auto Dimension = t_info.tensor_dimensions[idx];
-        if (idx == t_info.tensor_dimensions.size() - 1)
+        auto Dimension = t_info.dimensions[idx];
+        if (idx == t_info.dimensions.size() - 1)
         {
             os << t_info.dimension_to_string.at(t_info.order[idx]) << " : " << Dimension << "]" << std::endl;
         }
--- src/tensor_info.cpp
+++ src/tensor_info.cpp
@@ -2,20 +2,20 @@
 
 using TensorInfo = dai::TensorInfo;
 
-const std::map<TensorInfo::TensorDataType, unsigned int> TensorInfo::c_type_size = {
-    {TensorDataType::_fp16,     sizeof(float16)},
-    {TensorDataType::_u8f,      sizeof(std::uint8_t)},
-    {TensorDataType::_int,      sizeof(std::int32_t)},
-    {TensorDataType::_fp32,     sizeof(float)},
-    {TensorDataType::_i8,       sizeof(std::int8_t)},
+const std::map<TensorInfo::DataType, unsigned int> TensorInfo::c_type_size = {
+    {DataType::_fp16,     sizeof(float16)},
+    {DataType::_u8f,      sizeof(std::uint8_t)},
+    {DataType::_int,      sizeof(std::int32_t)},
+    {DataType::_fp32,     sizeof(float)},
+    {DataType::_i8,       sizeof(std::int8_t)},
 };
 
-const std::map<TensorInfo::TensorDataType, std::string> TensorInfo::type_to_string = {
-    {TensorDataType::_fp16,     "float16"},
-    {TensorDataType::_u8f,      "uint8"},
-    {TensorDataType::_int,      "int32"},
-    {TensorDataType::_fp32,     "float32"},
-    {TensorDataType::_i8,       "int8"},
+const std::map<TensorInfo::DataType, std::string> TensorInfo::type_to_string = {
+    {DataType::_fp16,     "float16"},
+    {DataType::_u8f,      "uint8"},
+    {DataType::_int,      "int32"},
+    {DataType::_fp32,     "float32"},
+    {DataType::_i8,       "int8"},
 };
 
 const std::map<TensorInfo::Dimension, std::string> TensorInfo::dimension_to_string = {
