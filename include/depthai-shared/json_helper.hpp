d00fdd7ca68878978f0eeb2191dc295ff2da4189
SzabolcsGergely <szabi@luxonis.com>
2020-10-05 21:13:08 +0300

    Replace macros w/ inline functions

--- include/depthai-shared/json_helper.hpp
+++ include/depthai-shared/json_helper.hpp
@@ -7,22 +7,17 @@
 
 
 
-// TODO
-// doesn't build on MacOS (clang), falling back to good old macros
-// template <typename T>
-// constexpr void DEPTHAI_SHARED_GET_JSON(T& variable, const nlohmann::json json)
-// {
-//     variable = json.get<T>();
-// }
+template <typename T>
+inline void DEPTHAI_SHARED_GET_JSON(T& variable, const nlohmann::json json)
+{
+    variable = json.get<T>();
+}
 
-// template <typename T>
-// constexpr void DEPTHAI_SHARED_SET_JSON(const T variable, nlohmann::json& json)
-// {
-//     json = variable;
-// }
-
-#define DEPTHAI_SHARED_GET_JSON(variable, json) variable = json.get<decltype(variable)>()
-#define DEPTHAI_SHARED_SET_JSON(variable, json) json = variable
+template <typename T>
+inline void DEPTHAI_SHARED_SET_JSON(const T variable, nlohmann::json& json)
+{
+    json = variable;
+}
 
 
 bool getJSONFromString(const std::string& str, nlohmann::json& json_obj);
