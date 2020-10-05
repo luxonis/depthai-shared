55b3527115b6f10fa17fb38019d1469766838e85
SzabolcsGergely <szabi@luxonis.com>
2020-10-05 21:02:44 +0300

    Fix build error w/ Clang on MacOS

--- include/depthai-shared/json_helper.hpp
+++ include/depthai-shared/json_helper.hpp
@@ -5,17 +5,24 @@
 
 #include "nlohmann/json.hpp"
 
-template <typename T>
-constexpr void DEPTHAI_SHARED_GET_JSON(T& variable, const nlohmann::json json)
-{
-    variable = json.get<T>();
-}
 
-template <typename T>
-constexpr void DEPTHAI_SHARED_SET_JSON(const T variable, nlohmann::json& json)
-{
-    json = variable;
-}
+
+// TODO
+// doesn't build on MacOS (clang), falling back to good old macros
+// template <typename T>
+// constexpr void DEPTHAI_SHARED_GET_JSON(T& variable, const nlohmann::json json)
+// {
+//     variable = json.get<T>();
+// }
+
+// template <typename T>
+// constexpr void DEPTHAI_SHARED_SET_JSON(const T variable, nlohmann::json& json)
+// {
+//     json = variable;
+// }
+
+#define DEPTHAI_SHARED_GET_JSON(variable, json) variable = json.get<decltype(variable)>()
+#define DEPTHAI_SHARED_SET_JSON(variable, json) json = variable
 
 
 bool getJSONFromString(const std::string& str, nlohmann::json& json_obj);
