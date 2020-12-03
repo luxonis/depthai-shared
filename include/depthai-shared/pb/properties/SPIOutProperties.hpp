052966747f53e06375d2b767020a5891bcc24124
jonngai <jon.ngai@gmail.com>
2020-12-03 00:43:21 -0700

    Adding SPI Node (#12)
    * Adding SPI protocol stuff.
    
    * Adding placeholder for SPIOutProperties.hpp.
    
    * Adding SPIOut.json template.
    
    * Adding busId to SPIOut interface.
    
    * Update comment.
    
    * Checking in style changes from clang-format.
    
    * Removing unused files.

---
+++ include/depthai-shared/pb/properties/SPIOutProperties.hpp
@@ -0,0 +1,29 @@
+//  To parse this JSON data, first install
+//
+//      Boost     http://www.boost.org
+//      json.hpp  https://github.com/nlohmann/json
+//
+//  Then include this file, and then do
+//
+//     SPIOutProperties.hpp data = nlohmann::json::parse(jsonString);
+
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+namespace dai {
+
+/**
+ * Properties for SPIOut which define stream name
+ */
+struct SPIOutProperties {
+    /**
+     * Set a limit to how many packets will be sent further to host
+     */
+    std::string streamName;
+    int busId;
+};
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(SPIOutProperties, streamName, busId)
+
+}  // namespace dai
