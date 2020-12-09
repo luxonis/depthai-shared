13a5b54d01248e48536f1d93c1e4e079093612d6
TheMarpe <martin.peterlin7@gmail.com>
2020-12-09 19:28:38 +0100

    Added NodeIoInfo for customizable nodes and input queue behaviour

--- include/depthai-shared/pb/NodeObjInfo.hpp
+++ include/depthai-shared/pb/NodeObjInfo.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
 #include <nlohmann/json.hpp>
+#include <vector>
 
+#include "NodeIoInfo.hpp"
 namespace dai {
 
 struct NodeObjInfo {
     int64_t id = -1;
     std::string name;
     nlohmann::json properties;
+    std::vector<NodeIoInfo> ioInfo;
 };
 
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(NodeObjInfo, id, name, properties)
