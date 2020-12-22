1110216ac601a1fc5b030f1d8cff35eaf5bde0be
TheMarpe <martin.peterlin7@gmail.com>
2020-12-22 21:34:50 +0100

    LogConstant pattern fix

--- include/depthai-shared/log/LogConstants.hpp
+++ include/depthai-shared/log/LogConstants.hpp
@@ -2,6 +2,6 @@
 
 namespace dai {
 
-constexpr static char* LOG_DEFAULT_PATTERN = "[%E.%e] [%n] [%l] %v";
+static constexpr const char* LOG_DEFAULT_PATTERN = "[%E.%e] [%n] [%^%l%$] %v";
 
 }  // namespace dai
