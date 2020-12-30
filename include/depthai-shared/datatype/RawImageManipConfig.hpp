d02fca177784b2c276cf2f867dc4556bd63ef6be
alex-luxonis <alex@luxonis.com>
2020-12-30 19:49:17 +0200

    ImageManip: add support for rotated/arbitrary cropping

--- include/depthai-shared/datatype/RawImageManipConfig.hpp
+++ include/depthai-shared/datatype/RawImageManipConfig.hpp
@@ -18,13 +18,63 @@ struct RawImageManipConfig : public RawBuffer {
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(CropRect, xmin, ymin, xmax, ymax);
     };
 
+    // Point2f, Size2f, RotatedRect are defined similar to OpenCV types
+    struct Point2f {
+        float x;
+        float y;
+
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE(Point2f, x, y);
+    };
+
+    struct Size2f {
+        float width;
+        float height;
+
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE(Size2f, width, height);
+    };
+
+    struct RotatedRect {
+        Point2f center;
+        Size2f size;
+        // degrees, increasing clockwise
+        float angle;
+
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE(RotatedRect, center, size, angle);
+    };
+
+    struct CropQuadrilateral {
+        // pt[0] is mapped to the top-left output corner, clockwise order
+        std::vector<Point2f> pt;
+        Size2f outSize;
+
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CropQuadrilateral, pt, outSize);
+    };
+
     struct CropConfig {
         CropRect cropRect;
+        RotatedRect cropRotatedRect;
+        CropQuadrilateral cropQuadrilateral;
+
         bool enableCenterCropRectangle = false;
         // if enableCenterCropRectangle -> automatically calculated crop parameters
         float cropRatio = 1.0f, widthHeightAspectRatio = 1.0f;
 
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CropConfig, cropRect, enableCenterCropRectangle, cropRatio, widthHeightAspectRatio);
+        bool enableRotatedRect = false;
+        bool enableCropQuadrilateral = false;
+
+        // Range 0..1 by default. Set 'false' to specify in pixels
+        bool normalizedCoords = true;
+
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CropConfig,
+                                       cropRect,
+                                       cropRotatedRect,
+                                       cropQuadrilateral,
+                                       enableCenterCropRectangle,
+                                       cropRatio,
+                                       widthHeightAspectRatio,
+                                       enableRotatedRect,
+                                       enableCropQuadrilateral,
+                                       normalizedCoords);
     };
 
     struct ResizeConfig {
