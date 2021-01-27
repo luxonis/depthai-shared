1abe0ee4423b22f1f9cf6e6a8c9e2c38c5d94b56
saching13 <sachin.guruswamy@gmail.com>
2021-01-27 10:55:56 -0800

    modified eeprom structure

--- include/depthai-shared/common/EepromData.hpp
+++ include/depthai-shared/common/EepromData.hpp
@@ -8,31 +8,49 @@
 
 namespace dai {
 
-struct EEPROMData {
+struct EepromData {
     // int64_t sec, nsec;
-    bool SwapLeftRightCam;
-    double MonoFovRad, RGBFovRad; // Can we change this to radians? most of the standard api's prefer radian. would make things easier to integrate
-    double LeftRightDistanceMtrs, LeftRGBDistanceMtrs; // since gen2 is more flexible can we move these to setting during pipeline creation or can be used from calibration
-    std::string name, revision; // board name. do we need this ? it might help in debugging which device the user is using.
-    /** 
+    bool swapLeftRightCam;
+    // Can we change this to radians? most of the standard api's prefer radian. would make things easier to integrate
+    double monoFovRad, rgbFovRad;
+    // since gen2 is more flexible can we move distance vars to setting during pipeline creation or can be used from calibration
+    double leftRightDistanceMtrs, leftRGBDistanceMtrs;
+    // board name. do we need this ? it might help in debugging which device the user is using.
+    std::string name, revision;
+    /**
      * While helping OPAL I realized this can also be produced rectification from the other information we get during calibration.
      * we might need to add that function on the host side if the users want to use rgb camera like OPAL did
-    **/
-    std::vector<std::vector<double>> RectifiedRotationLeft, RectifiedRotationRight;
+     **/
+    std::vector<std::vector<double>> rectifiedRotationLeft, rectifiedRotationRight;
     // Intriniscs of the cameras
-    std::vector<std::vector<double>> IntrinsicMatrixLeft, IntrinsicMatrixRight, IntrinsicMatrixRGB;
+    std::vector<std::vector<double>> intrinsicMatrixLeft, intrinsicMatrixRight, intrinsicMatrixRGB;
     // Relative rotation between cameras
-    std::vector<std::vector<double>> RotationMatrixLeftRight, RotationMatrixRightRGB; // need to add IMU extrinsics later
+    std::vector<std::vector<double>> rotationMatrixLeftRight, rotationMatrixRightRGB;  // need to add IMU extrinsics later
     // Relative translation between cameras
-    std::vector<double> TranslationLeftRight, TranslationRightRGB;
+    std::vector<double> translationLeftRight, translationRightRGB;
     // Distortion Coefficients
-    std::vector<double> DistortionCoeffLeft, DistortionCoeffRight, DistortionCoeffRGB;
+    std::vector<double> distortionCoeffLeft, distortionCoeffRight, distortionCoeffRGB;
 
-    NLOHMANN_DEFINE_TYPE_INTRUSIVE(EEPROMData, SwapLeftRightCam, MonoFovRad, RGBFovRad, LeftRightDistanceMtrs, \
-                                    LeftRGBDistanceMtrs, name, revision, RectifiedRotationLeft, RectifiedRotationRight,\
-                                    IntrinsicMatrixLeft, IntrinsicMatrixRight, IntrinsicMatrixRGB, \
-                                    RotationMatrixLeftRight, RotationMatrixRightRGB, TranslationLeftRight,\
-                                    TranslationRightRGB, DistortionCoeffLeft, DistortionCoeffRight, DistortionCoeffRGB);
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(EepromData,
+                                   swapLeftRightCam,
+                                   monoFovRad,
+                                   rgbFovRad,
+                                   leftRightDistanceMtrs,
+                                   leftRGBDistanceMtrs,
+                                   name,
+                                   revision,
+                                   rectifiedRotationLeft,
+                                   rectifiedRotationRight,
+                                   intrinsicMatrixLeft,
+                                   intrinsicMatrixRight,
+                                   intrinsicMatrixRGB,
+                                   rotationMatrixLeftRight,
+                                   rotationMatrixRightRGB,
+                                   translationLeftRight,
+                                   translationRightRGB,
+                                   distortionCoeffLeft,
+                                   distortionCoeffRight,
+                                   distortionCoeffRGB);
 };
 
-} // namespace dai
+}  // namespace dai
