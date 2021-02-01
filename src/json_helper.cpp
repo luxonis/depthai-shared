3a247b84e562ca1b71110cfdaf768fdffd3de78a
TheMarpe <martin.peterlin7@gmail.com>
2021-02-01 18:24:46 +0100

    Old file cleanup

--- src/json_helper.cpp
+++
@@ -1,8 +0,0 @@
-#include "depthai-shared/json_helper.hpp"
-
-#include <string>
-
-bool getJSONFromString(const std::string& str, nlohmann::json& json_obj) {
-    json_obj = nlohmann::json::parse(str);
-    return true;
-}
