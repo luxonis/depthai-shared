23d8da084f734ee9b045308bf9e230a6c6ef0467
Sachin <sachin.guruswamy@gmail.com>
2021-02-09 11:33:30 -0800

    added imu extrinsics

--- include/depthai-shared/common/EepromData.hpp
+++ include/depthai-shared/common/EepromData.hpp
@@ -13,9 +13,9 @@ namespace dai {
 
 struct point {
     float x, y, z;
-}
+};
 
-struct CameraExtrinsics {
+struct Extrinsics {
     std::vector<std::vector<double>> rotationMatrix;
     // (x, y, z) pose of destCameraSocket w.r.t currentCameraSocket obtained through calibration
     point translation;
@@ -33,14 +33,16 @@ struct StereoRectification {
 struct CameraInfo {
     std::vector<std::vector<double>> intrinsicMatrix;
     std::vector<double> distortionCoeff;
-    CameraExtrinsics extrinsics;
+    Extrinsics extrinsics;
 };
 
 struct EepromData {
     bool swapLeftRightCam;
     std::string name, revision;
-    std::unordered_map<CameraBoardSocket, CameraInfo> cameraData StereoRectification stereoRectificationData;
-}
+    std::unordered_map<CameraBoardSocket, CameraInfo> cameraData;
+    StereoRectification stereoRectificationData;
+    Extrinsics imuExtrinsics;
+};
 
 // struct EepromData {
 //     // int64_t sec, nsec;
