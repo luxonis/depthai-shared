8b6d3988782e763b0fcac72ff3de16730bcbe184
SzabolcsGergely <szabi@luxonis.com>
2021-02-21 17:42:07 +0200

    Disable exceptions if not supported

--- include/depthai-shared/datatype/RawDepthCalculatorConfig.hpp
+++ include/depthai-shared/datatype/RawDepthCalculatorConfig.hpp
@@ -18,11 +18,14 @@ struct Rect {
     Rect(){};
 
     void setRect(float xmin, float ymin, float xmax, float ymax) {
+#ifdef __cpp_exceptions
         if(xmin < 0 || xmin > 1) throw std::out_of_range("xmin out of range [0,1]");
         if(xmax < 0 || xmax > 1) throw std::out_of_range("xmax out of range [0,1]");
         if(ymin < 0 || ymin > 1) throw std::out_of_range("ymin out of range [0,1]");
         if(ymax < 0 || ymax > 1) throw std::out_of_range("ymax out of range [0,1]");
-
+        if(xmin >= xmax) throw std::out_of_range("xmin should be less than xmax");
+        if(ymin >= ymax) throw std::out_of_range("ymin should be less than ymax");
+#endif
         this->xmin = xmin;
         this->xmax = xmax;
         this->ymin = ymin;
