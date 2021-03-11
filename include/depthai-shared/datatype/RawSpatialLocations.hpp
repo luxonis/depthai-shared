703a130c75451d5fc1787dae78e9b5f3bc8c349b
SzabolcsGergely <szabi@luxonis.com>
2021-03-11 11:35:08 +0200

    Rename SpatialLocationsDataOut to SpatialLocations

--- include/depthai-shared/datatype/RawSpatialLocations.hpp
+++ include/depthai-shared/datatype/RawSpatialLocations.hpp
@@ -12,15 +12,15 @@
 
 namespace dai {
 
-struct SpatialLocationCalculatorDataOut {
+struct SpatialLocations {
     SpatialLocationCalculatorConfigData config;
     float depthAverage;
     Point3f spatialCoordinates;
 };
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(SpatialLocationCalculatorDataOut, config, depthAverage, spatialCoordinates);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(SpatialLocations, config, depthAverage, spatialCoordinates);
 
-struct RawSpatialLocationCalculatorData : public RawBuffer {
-    std::vector<SpatialLocationCalculatorDataOut> depth;
+struct RawSpatialLocations : public RawBuffer {
+    std::vector<SpatialLocations> spatialLocations;
 
     void serialize(std::vector<std::uint8_t>& metadata, DatatypeEnum& datatype) override {
         nlohmann::json j = *this;
@@ -28,7 +28,7 @@ struct RawSpatialLocationCalculatorData : public RawBuffer {
         datatype = DatatypeEnum::SpatialLocationCalculatorData;
     };
 
-    NLOHMANN_DEFINE_TYPE_INTRUSIVE(RawSpatialLocationCalculatorData, depth);
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(RawSpatialLocations, spatialLocations);
 };
 
 }  // namespace dai
