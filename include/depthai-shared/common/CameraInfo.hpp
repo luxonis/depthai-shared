e5fa0ddab2ae896fe76167fd12c15b92c3ebbb3f
Sachin <sachin.guruswamy@gmail.com>
2021-04-05 12:59:58 -0700

    Added camera model and removed matrix ops
    Signed-off-by: Sachin <sachin.guruswamy@gmail.com>

--- include/depthai-shared/common/CameraInfo.hpp
+++ include/depthai-shared/common/CameraInfo.hpp
@@ -1,17 +1,21 @@
 #pragma once
+
+#include "depthai-shared/common/CameraModel.hpp"
 #include "depthai-shared/common/Extrinsics.hpp"
 
 namespace dai {
 
 struct CameraInfo {
-    CameraInfo() : width(0), height(0), measuredFovDeg(0) {}
+    CameraInfo() : width(0), height(0), measuredFovDeg(0) {
+        cameraType = CameraModel::Perspective;
+    }
     uint16_t width, height;
     std::vector<std::vector<float>> intrinsicMatrix;
     std::vector<float> distortionCoeff;
     Extrinsics extrinsics;
     float measuredFovDeg;  // fov in deg
-    // TODO(sachin): Should I add type of distortion model here ?
-    NLOHMANN_DEFINE_TYPE_INTRUSIVE(CameraInfo, intrinsicMatrix, width, height, distortionCoeff, extrinsics, measuredFovDeg);
+    CameraModel cameraType;
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(CameraInfo, cameraType, width, height, measuredFovDeg, intrinsicMatrix, distortionCoeff, extrinsics);
 };
 
 }  // namespace dai
