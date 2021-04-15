f0d731da966c542fe57e81cc926f99601c8bb958
alex-luxonis <alex@luxonis.com>
2021-04-16 00:38:45 +0300

    Add options for fixed mic gain, experimental AGC

--- include/depthai-shared/properties/UACProperties.hpp
+++ include/depthai-shared/properties/UACProperties.hpp
@@ -13,8 +13,18 @@ struct UACProperties {
      * Front L/R by default
      */
     bool streamBackMic = false;
+
+    /**
+     * Experimental, control the gain automatically (digital AGC)
+     */
+    bool enableAgc = false;
+
+    /**
+     * Configurable fixed mic gain (x times)
+     */
+    float micGain = 1.0;
 };
 
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(UACProperties, streamBackMic);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(UACProperties, streamBackMic, enableAgc, micGain);
 
 }  // namespace dai
