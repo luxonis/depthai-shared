ffd0fb861e30f2a6708725c6562f9b398b52f518
Sachin <sachin.guruswamy@gmail.com>
2021-04-26 11:34:07 -0700

    added hash function

--- include/depthai-shared/common/CameraBoardSocket.hpp
+++ include/depthai-shared/common/CameraBoardSocket.hpp
@@ -2,6 +2,7 @@
 
 #include <nlohmann/json.hpp>
 
+
 namespace dai {
 /**
  * Which Camera socket to use.
@@ -11,3 +12,15 @@ namespace dai {
 enum class CameraBoardSocket : int32_t { AUTO = -1, RGB, LEFT, RIGHT };
 
 }  // namespace dai
+
+namespace std {
+
+template <>
+struct hash<dai::CameraBoardSocket> {
+    std::size_t operator()(const dai::CameraBoardSocket& s) const {
+        using Type = typename underlying_type<dai::CameraBoardSocket>::type;
+        return std::hash<Type>{}(static_cast<Type>(s));
+    }
+};
+
+}  // namespace std
