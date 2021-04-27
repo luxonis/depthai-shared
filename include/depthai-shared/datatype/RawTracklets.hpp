18cb4a5fde3dda784080025b31812f02b7d2475d
SzabolcsGergely <szabi@luxonis.com>
2021-04-28 02:46:25 +0300

    Apply formatting

--- include/depthai-shared/datatype/RawTracklets.hpp
+++ include/depthai-shared/datatype/RawTracklets.hpp
@@ -18,8 +18,8 @@ struct Tracklet {
     enum class TrackingStatus : std::int32_t {
         NEW,     /**< The object is newly added. */
         TRACKED, /**< The object is being tracked. */
-        LOST, /**< The object gets lost now. The object can be tracked again automatically(long term tracking) or by specifying detected object manually(short
-                term and zero term tracking). */
+        LOST,   /**< The object gets lost now. The object can be tracked again automatically(long term tracking) or by specifying detected object manually(short
+                  term and zero term tracking). */
         REMOVED /**< The object is removed. */
     };
     /**
