99a3c2ba20cb92eb4226237790b202a86f30192f
alex-luxonis <alex@luxonis.com>
2021-04-28 17:34:32 +0300

    UACProperties: add several new fields

--- include/depthai-shared/properties/UACProperties.hpp
+++ include/depthai-shared/properties/UACProperties.hpp
@@ -23,8 +23,47 @@ struct UACProperties {
      * Configurable fixed mic gain (x times)
      */
     float micGain = 1.0;
+
+    /**
+     * Audio sample rate
+     */
+    uint32_t sampleRateHz = 48000;
+
+    /**
+     * I2S read size [samples]. Set a lower value to reduce latency
+     */
+    uint32_t i2sReadSizeSamples = 1440;
+
+    /**
+     * XLink send size [samples]
+     */
+    uint32_t xlinkSendSizeSamples = 1440;
+
+    /**
+     * XLink send interleaved samples (from all mics), as opposed to "planar"
+     */
+    bool xlinkSendInterleaved = true;
+
+    /**
+     * Apply the configured `micGain` for XLink output as well (not only UAC)
+     */
+    bool xLinkApplyMicGain = true;
+
+    /**
+     * Native MIC data is 24-bit. Optionally change to 16-bit or 32-bit
+     */
+    uint8_t xlinkSampleSizeBytes = 3;
 };
 
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(UACProperties, streamBackMic, enableAgc, micGain);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(UACProperties,
+                                   streamBackMic,
+                                   enableAgc,
+                                   micGain,
+                                   sampleRateHz,
+                                   i2sReadSizeSamples,
+                                   xlinkSendSizeSamples,
+                                   xlinkSendInterleaved,
+                                   xLinkApplyMicGain,
+                                   xlinkSampleSizeBytes);
 
 }  // namespace dai
