9de02dfe5bdaee01362ce72bf41516b6b7fcbe1a
TheMarpe <martin.peterlin7@gmail.com>
2021-07-23 01:22:56 +0200

    Updated SPIIn properties

--- include/depthai-shared/properties/SPIInProperties.hpp
+++ include/depthai-shared/properties/SPIInProperties.hpp
@@ -17,10 +17,25 @@ namespace dai {
  * Properties for SPIIn node
  */
 struct SPIInProperties {
-    /// Output stream name
+    /**
+     * Name of stream
+     */
     std::string streamName;
-    /// SPI bus to use
+
+    /**
+     * SPI bus to use
+     */
     int busId;
+
+    /**
+     * Maximum input data size
+     */
+    std::uint32_t maxDataSize = dai::XLINK_USB_BUFFER_MAX_SIZE;
+
+    /**
+     * Number of frames in pool
+     */
+    std::uint32_t numFrames = 4;
 };
 
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(SPIInProperties, streamName, busId);
