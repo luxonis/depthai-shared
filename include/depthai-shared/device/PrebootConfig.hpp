68e2c7acbefd24ef4af736d67616663a017fe4f8
TheMarpe <martin.peterlin7@gmail.com>
2021-07-22 12:17:50 +0200

    Grouped preboot config options

--- include/depthai-shared/device/PrebootConfig.hpp
+++ include/depthai-shared/device/PrebootConfig.hpp
@@ -13,9 +13,14 @@ constexpr static uint32_t PREBOOT_CONFIG_MAGIC1 = 0x78010000U;
 constexpr static uint32_t PREBOOT_CONFIG_MAGIC2 = 0x21ea17e6U;
 
 struct PrebootConfig {
-    uint16_t vid = 0x03e7, pid = 0xf63b;
-    uint16_t flashBootedVid = 0x03e7, flashBootedPid = 0xf63d;
-    UsbSpeed maxUsbSpeed = UsbSpeed::SUPER;
+    // USB related config
+    struct USB {
+        uint16_t vid = 0x03e7, pid = 0xf63b;
+        uint16_t flashBootedVid = 0x03e7, flashBootedPid = 0xf63d;
+        UsbSpeed maxSpeed = UsbSpeed::SUPER;
+    };
+
+    USB usb;
     uint32_t watchdogTimeoutMs = device::XLINK_WATCHDOG_TIMEOUT.count();
 };
 
