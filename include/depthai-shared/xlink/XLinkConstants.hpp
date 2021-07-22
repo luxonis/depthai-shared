07ecb24a7f57c64f82e43ed295712d7113d06eea
TheMarpe <martin.peterlin7@gmail.com>
2021-07-22 12:47:41 +0200

    Added a separate watchdog stream

--- include/depthai-shared/xlink/XLinkConstants.hpp
+++ include/depthai-shared/xlink/XLinkConstants.hpp
@@ -11,6 +11,7 @@ constexpr static const char* XLINK_CHANNEL_PIPELINE_CONFIG = "__pipeline_config"
 constexpr static const char* XLINK_CHANNEL_MAIN_RPC = "__rpc_main";
 constexpr static const char* XLINK_CHANNEL_TIMESYNC = "__timesync";
 constexpr static const char* XLINK_CHANNEL_LOG = "__log";
+constexpr static const char* XLINK_CHANNEL_WATCHDOG = "__watchdog";
 
 // usb buffer maximum size
 constexpr static std::uint32_t XLINK_USB_BUFFER_MAX_SIZE = 5 * 1024 * 1024;
