13593fff1614e4f925a5d1e8a07c5f1673a2d6be
SzabolcsGergely <szabi@luxonis.com>
2021-07-26 11:59:14 +0300

    Add configurable shave/cmx slices

--- include/depthai-shared/properties/FeatureTrackerProperties.hpp
+++ include/depthai-shared/properties/FeatureTrackerProperties.hpp
@@ -11,12 +11,28 @@ namespace dai {
  * Specify properties for FeatureTracker
  */
 struct FeatureTrackerProperties {
+    /// Initial feature tracker config
     RawFeatureTrackerConfig initialConfig;
 
     /// Whether to wait for config at 'inputConfig' IO
     bool inputConfigSync = false;
+
+    /**
+     * Number of shaves reserved for feature tracking.
+     * Optical flow can use 1 or 2 shaves, while for corner detection only 1 is enough.
+     * Maximum 2, minimum 1.
+     */
+    std::int32_t numShaves = 1;
+
+    /**
+     * Number of memory slices reserved for feature tracking.
+     * Optical flow can use 1 or 2 memory slices, while for corner detection only 1 is enough.
+     * Maximum number of features depends on the number of allocated memory slices.
+     * Maximum 2, minimum 1.
+     */
+    std::int32_t numMemorySlices = 1;
 };
 
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(FeatureTrackerProperties, initialConfig, inputConfigSync);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(FeatureTrackerProperties, initialConfig, inputConfigSync, numShaves, numMemorySlices);
 
 }  // namespace dai
