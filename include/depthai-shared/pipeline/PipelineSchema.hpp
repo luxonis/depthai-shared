05ddbece00345a1c8808cedd66226761bf9033ec
TheMarpe <martin.peterlin7@gmail.com>
2021-10-17 15:42:42 +0200

    Removed old comments

--- include/depthai-shared/pipeline/PipelineSchema.hpp
+++ include/depthai-shared/pipeline/PipelineSchema.hpp
@@ -1,12 +1,3 @@
-//  To parse this JSON data, first install
-//
-//      Boost     http://www.boost.org
-//      json.hpp  https://github.com/nlohmann/json
-//
-//  Then include this file, and then do
-//
-//     PipelineSchema.hpp data = nlohmann::json::parse(jsonString);
-
 #pragma once
 
 #include "NodeConnectionSchema.hpp"
