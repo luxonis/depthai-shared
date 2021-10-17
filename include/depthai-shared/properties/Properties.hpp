2694e047720a0bdbb3481f05c3c589b590a04db6
TheMarpe <martin.peterlin7@gmail.com>
2021-10-17 21:14:21 +0200

    Added clone to Properties

--- include/depthai-shared/properties/Properties.hpp
+++ include/depthai-shared/properties/Properties.hpp
@@ -6,15 +6,20 @@ namespace dai {
 
 /// Base Properties structure
 struct Properties {
-    virtual void serialize(std::vector<std::uint8_t>& data) = 0;
+    virtual void serialize(std::vector<std::uint8_t>& data) const = 0;
+    virtual std::unique_ptr<Properties> clone() const = 0;
     virtual ~Properties() = default;
 };
 
 /// Serializable properties
 template <typename Base, typename Derived>
 struct PropertiesSerializable : Base {
-    virtual void serialize(std::vector<std::uint8_t>& data) override {
+    virtual void serialize(std::vector<std::uint8_t>& data) const override {
         utility::serialize(static_cast<const Derived&>(*this), data);
+    }
+
+    virtual std::unique_ptr<Properties> clone() const override {
+        return std::make_unique<Derived>(static_cast<const Derived&>(*this));
     }
 };
 
