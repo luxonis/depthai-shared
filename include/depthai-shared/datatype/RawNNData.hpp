9003ea719c4a7655c0778eaf96460152cb40c922
TheMarpe <martin.peterlin7@gmail.com>
2021-12-24 16:09:51 +0100

    Added sequence and timestamp to NNData

--- include/depthai-shared/datatype/RawNNData.hpp
+++ include/depthai-shared/datatype/RawNNData.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "depthai-shared/datatype/RawBuffer.hpp"
+#include "depthai-shared/common/Timestamp.hpp"
 #include "depthai-shared/utility/Serialization.hpp"
 
 namespace dai {
@@ -49,12 +50,17 @@ struct RawNNData : public RawBuffer {
     std::vector<TensorInfo> tensors;
     unsigned int batchSize;
 
+    // Related to input ImgFrame
+    int sequenceNum;     // increments for each frame
+    Timestamp ts;        // generation timestamp, synced to host time
+    Timestamp tsDevice;  // generation timestamp, direct device monotonic clock
+
     void serialize(std::vector<std::uint8_t>& metadata, DatatypeEnum& datatype) const override {
         metadata = utility::serialize(*this);
         datatype = DatatypeEnum::NNData;
     };
 
-    DEPTHAI_SERIALIZE(RawNNData, tensors, batchSize);
+    DEPTHAI_SERIALIZE(RawNNData, tensors, batchSize, sequenceNum, ts, tsDevice);
 };
 
 }  // namespace dai
