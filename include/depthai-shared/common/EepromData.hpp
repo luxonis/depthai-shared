1fbe1a5842b07ee2d1a148e920c51de92726205b
TheMarpe <martin.peterlin7@gmail.com>
2022-04-21 12:04:35 +0200

    Added additional data available in EEPROM version 7

--- include/depthai-shared/common/EepromData.hpp
+++ include/depthai-shared/common/EepromData.hpp
@@ -17,13 +17,14 @@ namespace dai {
  * Contains the Calibration and Board data stored on device
  */
 struct EepromData {
-    uint32_t version = 6;
-    std::string boardName, boardRev;
+    uint32_t version = 7;
+    std::string productName, boardName, boardRev, boardId, hardwareId, batchName;
+    uint64_t batchTime;
     std::unordered_map<CameraBoardSocket, CameraInfo> cameraData;
     StereoRectification stereoRectificationData;
     Extrinsics imuExtrinsics;
     std::vector<uint8_t> miscellaneousData;
-    DEPTHAI_SERIALIZE(EepromData, version, boardName, boardRev, cameraData, stereoRectificationData, imuExtrinsics, miscellaneousData);
+    DEPTHAI_SERIALIZE(EepromData, version, boardName, boardRev, boardId, hardwareId, batchName, batchTime, cameraData, stereoRectificationData, imuExtrinsics, miscellaneousData);
 };
 
 }  // namespace dai
