2c82a2e6ca30b93be399043764d70dc3ba1696bf
TheMarpe <martin.peterlin7@gmail.com>
2022-04-21 12:04:35 +0200

    Added customBoard EEPROM field

--- include/depthai-shared/common/EepromData.hpp
+++ include/depthai-shared/common/EepromData.hpp
@@ -18,13 +18,13 @@ namespace dai {
  */
 struct EepromData {
     uint32_t version = 7;
-    std::string productName, boardName, boardRev, boardId, hardwareId, batchName;
-    uint64_t batchTime;
+    std::string productName, boardName, boardRev, boardId, hardwareId, customBoard, batchName;
+    uint64_t batchTime{0};
     std::unordered_map<CameraBoardSocket, CameraInfo> cameraData;
     StereoRectification stereoRectificationData;
     Extrinsics imuExtrinsics;
     std::vector<uint8_t> miscellaneousData;
-    DEPTHAI_SERIALIZE(EepromData, version, boardName, boardRev, boardId, hardwareId, batchName, batchTime, cameraData, stereoRectificationData, imuExtrinsics, miscellaneousData);
+    DEPTHAI_SERIALIZE(EepromData, version, boardName, boardRev, boardId, hardwareId, customBoard, batchName, batchTime, cameraData, stereoRectificationData, imuExtrinsics, miscellaneousData);
 };
 
 }  // namespace dai
