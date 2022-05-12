ad0d1572b9f79c25169ab19d29737564affb7745
SzabolcsGergely <szabi@luxonis.com>
2022-05-12 22:51:57 +0300

    Set firmware update behavior to false by default

--- include/depthai-shared/properties/IMUProperties.hpp
+++ include/depthai-shared/properties/IMUProperties.hpp
@@ -171,10 +171,11 @@ struct IMUProperties : PropertiesSerializable<Properties, IMUProperties> {
     std::int32_t batchReportThreshold = 1;
     /* Maximum number of IMU packets in a batch. Maximum 5. */
     std::int32_t maxBatchReports = 5;
-    /* By default the behavior is automatic, it will perform firmware update if needed.
-     * Optionally the behavior can be enforced.
+    /*
+     * Whether to perform firmware update or not.
+     * Default value: false.
      */
-    tl::optional<bool> enableFirmwareUpdate;
+    tl::optional<bool> enableFirmwareUpdate = false;
 };
 
 DEPTHAI_SERIALIZE_EXT(IMUProperties, imuSensors, batchReportThreshold, maxBatchReports, enableFirmwareUpdate);
