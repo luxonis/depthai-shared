19ff61be8b99a419863eed06a0fd6e81de39bf6c
Martin Peterlin <martin.peterlin7@gmail.com>
2022-05-15 18:47:10 +0200

    WIP: DetectionParser node

--- include/depthai-shared/properties/DetectionNetworkProperties.hpp
+++ include/depthai-shared/properties/DetectionNetworkProperties.hpp
@@ -5,7 +5,7 @@
 
 // project
 #include "NeuralNetworkProperties.hpp"
-#include "depthai-shared/common/DetectionNetworkType.hpp"
+#include "depthai-shared/common/DetectionParserOptions.hpp"
 #include "depthai-shared/common/optional.hpp"
 
 namespace dai {
@@ -14,30 +14,16 @@ namespace dai {
  *  Specify properties for DetectionNetwork
  */
 struct DetectionNetworkProperties : PropertiesSerializable<NeuralNetworkProperties, DetectionNetworkProperties> {
-    /// Generic Neural Network properties
-    DetectionNetworkType nnFamily;
-    float confidenceThreshold;
-
-    /// YOLO specific network properties
-    int classes;
-    int coordinates;
-    std::vector<float> anchors;
-    std::map<std::string, std::vector<int>> anchorMasks;
-    float iouThreshold;
+    DetectionParserOptions parser;
 };
 
 DEPTHAI_SERIALIZE_EXT(DetectionNetworkProperties,
-                      nnFamily,
                       blobSize,
                       blobUri,
                       numFrames,
                       numThreads,
                       numNCEPerThread,
-                      confidenceThreshold,
-                      classes,
-                      coordinates,
-                      anchors,
-                      anchorMasks,
-                      iouThreshold);
+                      parser
+                      );
 
 }  // namespace dai
