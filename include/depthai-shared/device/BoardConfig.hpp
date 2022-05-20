ab03413f4358a435c8e8a829184dcd144320defa
TheMarpe <martin.peterlin7@gmail.com>
2022-05-20 20:16:39 +0200

    Updated style

--- include/depthai-shared/device/BoardConfig.hpp
+++ include/depthai-shared/device/BoardConfig.hpp
@@ -7,9 +7,9 @@
 // project
 #include "depthai-shared/common/UsbSpeed.hpp"
 #include "depthai-shared/common/optional.hpp"
+#include "depthai-shared/log/LogLevel.hpp"
 #include "depthai-shared/utility/Serialization.hpp"
 #include "depthai-shared/xlink/XLinkConstants.hpp"
-#include "depthai-shared/log/LogLevel.hpp"
 
 namespace dai {
 
@@ -104,6 +104,19 @@ DEPTHAI_SERIALIZE_EXT(BoardConfig::USB, vid, pid, flashBootedVid, flashBootedPid
 DEPTHAI_SERIALIZE_EXT(BoardConfig::Network, mtu, xlinkTcpNoDelay);
 DEPTHAI_SERIALIZE_EXT(BoardConfig::GPIO, mode, direction, level, pull, drive, schmitt, slewFast);
 DEPTHAI_SERIALIZE_EXT(BoardConfig::UART, tmp);
-DEPTHAI_SERIALIZE_EXT(BoardConfig, usb, network, sysctl, watchdogTimeoutMs, watchdogInitialDelayMs, gpio, uart, pcieInternalClock, usb3PhyInternalClock, emmc, logPath, logSizeMax, logVerbosity);
+DEPTHAI_SERIALIZE_EXT(BoardConfig,
+                      usb,
+                      network,
+                      sysctl,
+                      watchdogTimeoutMs,
+                      watchdogInitialDelayMs,
+                      gpio,
+                      uart,
+                      pcieInternalClock,
+                      usb3PhyInternalClock,
+                      emmc,
+                      logPath,
+                      logSizeMax,
+                      logVerbosity);
 
 }  // namespace dai
