5cc31675f36a35a7dd4cf2bce1d74c6bf98a88f5
alex-luxonis <alex@luxonis.com>
2022-07-07 17:22:05 +0300

    AudioProcProperties: add `sampleRate` field

--- include/depthai-shared/properties/AudioProcProperties.hpp
+++ include/depthai-shared/properties/AudioProcProperties.hpp
@@ -19,8 +19,15 @@ struct AudioProcProperties : PropertiesSerializable<Properties, AudioProcPropert
      * Value "0" indicates automatic number of frames assignment
      */
     std::uint32_t numFramesPool = 0;
+
+    /**
+     * Sample rate to pre-process the incoming data. AudioProc needs the sound
+     * data downsampled to 16kHz first, otherwise the performance may be affected.
+     * Values different than 16000 are experimental. Allowed for now: 16000, 24000, 48000
+     */
+    int sampleRate = 16000;
 };
 
-DEPTHAI_SERIALIZE_EXT(AudioProcProperties, initialConfig, numFramesPool);
+DEPTHAI_SERIALIZE_EXT(AudioProcProperties, initialConfig, numFramesPool, sampleRate);
 
 }  // namespace dai
