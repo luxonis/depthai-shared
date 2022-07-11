6b14f7b7e2f2fa0d4d36a216c854c1a1a58fcc83
alex-luxonis <alex@luxonis.com>
2022-07-11 15:19:15 +0300

    ColorCameraProperties: add 720p and 800p resolutions for OV9782

--- include/depthai-shared/properties/ColorCameraProperties.hpp
+++ include/depthai-shared/properties/ColorCameraProperties.hpp
@@ -25,7 +25,7 @@ struct ColorCameraProperties : PropertiesSerializable<Properties, ColorCameraPro
     /**
      * Select the camera sensor resolution
      */
-    enum class SensorResolution : int32_t { THE_1080_P, THE_4_K, THE_12_MP, THE_13_MP };
+    enum class SensorResolution : int32_t { THE_1080_P, THE_4_K, THE_12_MP, THE_13_MP, THE_720_P, THE_800_P };
 
     /**
      * For 24 bit color these can be either RGB or BGR
