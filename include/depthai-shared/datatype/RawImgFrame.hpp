5bd576011c86d3828dfc23f0e36c79e910cd220f
Matevz Morato <matevz.morato@gmail.com>
2022-10-27 02:09:01 +0200

    Add support functions to ImgTransformation

--- include/depthai-shared/datatype/RawImgFrame.hpp
+++ include/depthai-shared/datatype/RawImgFrame.hpp
@@ -2,9 +2,9 @@
 
 #include <unordered_map>
 
+#include "depthai-shared/common/ImgTransformation.hpp"
 #include "depthai-shared/common/Timestamp.hpp"
 #include "depthai-shared/datatype/RawBuffer.hpp"
-#include "depthai-shared/common/RawImgTransformation.hpp"
 #include "depthai-shared/utility/Serialization.hpp"
 
 namespace dai {
@@ -185,14 +185,14 @@ struct RawImgFrame : public RawBuffer {
     int64_t sequenceNum = 0;   // increments for each frame
     Timestamp ts = {};         // generation timestamp, synced to host time
     Timestamp tsDevice = {};   // generation timestamp, direct device monotonic clock
-    RawImgTransformation transformationData = {};
+    ImgTransformation transformation;
 
     void serialize(std::vector<std::uint8_t>& metadata, DatatypeEnum& datatype) const override {
         metadata = utility::serialize(*this);
         datatype = DatatypeEnum::ImgFrame;
     };
 
-    DEPTHAI_SERIALIZE(RawImgFrame, fb, sourceFb, cam, category, instanceNum, sequenceNum, ts, tsDevice, transformationData);
+    DEPTHAI_SERIALIZE(RawImgFrame, fb, sourceFb, cam, category, instanceNum, sequenceNum, ts, tsDevice, transformation);
 };
 
 }  // namespace dai
