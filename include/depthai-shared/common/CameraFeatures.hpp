8702c865f8bd6fc78a2a0ef0c40ee3ee9a88f828
TheMarpe <martin.peterlin7@gmail.com>
2022-11-29 15:01:01 +0100

    Added some default values to CameraFeatures

--- include/depthai-shared/common/CameraFeatures.hpp
+++ include/depthai-shared/common/CameraFeatures.hpp
@@ -16,7 +16,7 @@ struct CameraFeatures {
     /**
      * Board socket where the camera was detected
      */
-    CameraBoardSocket socket;
+    CameraBoardSocket socket = CameraBoardSocket::AUTO;
     /**
      * Camera sensor name, e.g: "IMX378", "OV9282"
      */
@@ -24,11 +24,11 @@ struct CameraFeatures {
     /**
      * Maximum sensor resolution
      */
-    std::int32_t width, height;
+    std::int32_t width = -1, height = -1;
     /**
      * Default camera orientation, board dependent
      */
-    CameraImageOrientation orientation;
+    CameraImageOrientation orientation = CameraImageOrientation::AUTO;
     /**
      * List of supported types of processing for the given camera.
      *
@@ -39,7 +39,7 @@ struct CameraFeatures {
     /**
      *  Whether an autofocus VCM IC was detected
      */
-    bool hasAutofocus;
+    bool hasAutofocus = false;
     /**
      * Camera name or alias
      */
