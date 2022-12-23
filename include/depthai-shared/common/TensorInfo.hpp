95cbc8c0ea3145278235e7b160ef3cf24b2240f1
Matevz Morato <matevz.morato@gmail.com>
2022-12-23 18:36:44 +0100

    Add convience functions to tensor info

--- include/depthai-shared/common/TensorInfo.hpp
+++ include/depthai-shared/common/TensorInfo.hpp
@@ -35,6 +35,118 @@ struct TensorInfo {
         I8 = 4,    // Signed byte
     };
 
+    int getDataTypeSize() {
+        switch(dataType) {
+            case DataType::U8F:
+            case DataType::I8:
+                return sizeof(uint8_t);
+            case DataType::FP16:
+                return sizeof(uint16_t);
+            case DataType::INT:
+            case DataType::FP32:
+                return sizeof(float);
+            default:
+                return 0;
+                break;
+        }
+    }
+
+    int getWidth() {
+        // TODO Don't assume that dims are big enough
+        switch(order) {
+            case StorageOrder::NHWC:
+                return dims[2];
+            case StorageOrder::NHCW:
+                return dims[3];
+            case StorageOrder::NCHW:
+                return dims[3];
+            case StorageOrder::HWC:
+                return dims[1];
+            case StorageOrder::CHW:
+                return dims[2];
+            case StorageOrder::WHC:
+                return dims[1];
+            case StorageOrder::HCW:
+                return dims[2];
+            case StorageOrder::WCH:
+                return dims[0];
+            case StorageOrder::CWH:
+                return dims[1];
+            case StorageOrder::W:
+                return dims[0];
+            case StorageOrder::NC:
+            case StorageOrder::CN:
+            case StorageOrder::C:
+            case StorageOrder::H:
+            default:
+                return 0;
+        }
+    }
+
+    int getHeight() {
+        switch(order) {
+            case StorageOrder::NHWC:
+                return dims[1];
+            case StorageOrder::NHCW:
+                return dims[1];
+            case StorageOrder::NCHW:
+                return dims[2];
+            case StorageOrder::HWC:
+                return dims[0];
+            case StorageOrder::CHW:
+                return dims[1];
+            case StorageOrder::WHC:
+                return dims[1];
+            case StorageOrder::HCW:
+                return dims[0];
+            case StorageOrder::WCH:
+                return dims[2];
+            case StorageOrder::CWH:
+                return dims[2];
+            case StorageOrder::H:
+                return dims[0];
+            case StorageOrder::NC:
+            case StorageOrder::CN:
+            case StorageOrder::C:
+            case StorageOrder::W:
+            default:
+                return 0;
+        }
+    }
+
+    int getChannels() {
+         switch(order) {
+            case StorageOrder::NHWC:
+                return dims[3];
+            case StorageOrder::NHCW:
+                return dims[2];
+            case StorageOrder::NCHW:
+                return dims[3];
+            case StorageOrder::HWC:
+                return dims[2];
+            case StorageOrder::CHW:
+                return dims[0];
+            case StorageOrder::WHC:
+                return dims[2];
+            case StorageOrder::HCW:
+                return dims[1];
+            case StorageOrder::WCH:
+                return dims[1];
+            case StorageOrder::CWH:
+                return dims[0];
+            case StorageOrder::C:
+                return dims[0];
+            case StorageOrder::NC:
+                return dims[1];
+            case StorageOrder::CN:
+                return dims[0];
+            case StorageOrder::H:
+            case StorageOrder::W:
+            default:
+                return 0;
+        }
+   }
+
     StorageOrder order = StorageOrder::NCHW;
     DataType dataType = DataType::FP16;
     unsigned int numDimensions = 0;
