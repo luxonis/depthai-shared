1219ddab580c5e2f93db734c6a57ca24661412bd
SzabolcsGergely <szabi@luxonis.com>
2023-02-10 18:24:23 +0200

    Remove unused enum

--- include/depthai-shared/device/CrashDump.hpp
+++ include/depthai-shared/device/CrashDump.hpp
@@ -43,36 +43,6 @@ struct CrashDump {
         ErrorSourceInfo errorSourceInfo;
 
         struct ThreadCallstack {
-            enum class ThreadStatus : uint32_t {
-                READY = 0x00000000,
-                WAITING_FOR_MUTEX = 0x00000001,
-                WAITING_FOR_SEMAPHORE = 0x00000002,
-                WAITING_FOR_EVENT = 0x00000004,
-                WAITING_FOR_SYSTEM_EVENT = 0x00000008,
-                WAITING_FOR_MESSAGE = 0x00000010,
-                WAITING_FOR_CONDITION_VARIABLE = 0x00000020,
-                WAITING_FOR_FUTEX = 0x00000040,
-                WAITING_FOR_BSD_WAKEUP = 0x00000080,
-                WAITING_FOR_TIME = 0x00000100,
-                WAITING_FOR_PERIOD = 0x00000200,
-                WAITING_FOR_SIGNAL = 0x00000400,
-                WAITING_FOR_BARRIER = 0x00000800,
-                WAITING_FOR_RWLOCK = 0x00001000,
-                WAITING_FOR_JOIN_AT_EXIT = 0x00002000,
-                WAITING_FOR_JOIN = 0x00004000,
-                SUSPENDED = 0x00008000,
-                WAITING_FOR_SEGMENT = 0x00010000,
-                LIFE_IS_CHANGING = 0x00020000,
-                DEBUGGER = 0x08000000,
-                INTERRUPTIBLE_BY_SIGNAL = 0x10000000,
-                WAITING_FOR_RPC_REPLY = 0x20000000,
-                ZOMBIE = 0x40000000,
-                DORMANT = 0x80000000,
-                LOCALLY_BLOCKED = 0x00006CF3,
-                BLOCKED = 0x30006FFF,
-                ALL_SET = 0xFFFFFFFF
-            };
-
             uint32_t threadId;
             std::string threadName;
             uint32_t stackBottom;
