93d0146b4ce3afc1bb81ff05064520f8b595bdd3
Matevz Morato <matevz.morato@gmail.com>
2023-06-21 18:26:55 +0000

    Add an option to explicitly define a point to be optional or not

--- include/depthai-shared/common/Point2f.hpp
+++ include/depthai-shared/common/Point2f.hpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 
 // project
+#include "depthai-shared/common/optional.hpp"
 #include "depthai-shared/utility/Serialization.hpp"
 
 namespace dai {
@@ -15,14 +16,19 @@ namespace dai {
  */
 struct Point2f {
     Point2f() = default;
-    Point2f(float x, float y) {
+    Point2f(float x, float y, tl::optional<bool> normalized = tl::nullopt) {
         this->x = x;
         this->y = y;
+        this->normalized = normalized;
     }
     float x = 0, y = 0;
+    tl::optional<bool> normalized = tl::nullopt;
 
     bool isNormalized() const {
-        // When ambiguous, default to denormalized - this is to avoid breaking existing code TODO(refactor)
+        if(normalized) {
+            return *normalized;
+        }
+        // When ambiguous, default to denormalized
         if((x == 0 || x == 1) && (y == 0 || y == 1)) return false;
         return x >= 0 && x <= 1 && y >= 0 && y <= 1;
     }
