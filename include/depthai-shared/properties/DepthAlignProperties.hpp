720872b0d942658fa0a04093020f49b6d2a5a60d
Andrej Susnik <andrej.susnik@luxonis.com>
2023-06-26 12:27:23 +0200

    Add aditional properties to depth align node

--- include/depthai-shared/properties/DepthAlignProperties.hpp
+++ include/depthai-shared/properties/DepthAlignProperties.hpp
@@ -14,9 +14,14 @@ namespace dai {
  */
 struct DepthAlignProperties: PropertiesSerializable<Properties, DepthAlignProperties> {
     RawDepthAlignConfig initialConfig;
+    CameraBoardSocket alignFrom = CameraBoardSocket::AUTO;
     CameraBoardSocket alignTo = CameraBoardSocket::AUTO;
+    int alignWidth = 1280;
+    int alignHeight = 720;
+
+    bool downscaleSource = true;
 };
 
-DEPTHAI_SERIALIZE_EXT(DepthAlignProperties, initialConfig, alignTo);
+DEPTHAI_SERIALIZE_EXT(DepthAlignProperties, initialConfig, alignFrom, alignTo);
 
 }  // namespace dai
