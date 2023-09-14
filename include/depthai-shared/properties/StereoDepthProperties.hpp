38dc9c57039bcc514791c691a92e84fe753d3627
asahtik <asahtik@gmail.com>
2023-09-14 12:22:19 +0200

    Changed use spec to optional

--- include/depthai-shared/properties/StereoDepthProperties.hpp
+++ include/depthai-shared/properties/StereoDepthProperties.hpp
@@ -160,21 +160,21 @@ struct StereoDepthProperties : PropertiesSerializable<Properties, StereoDepthPro
      * Suitable for debugging.
      * Default: true
      */
-    tl::optional<bool> disparityToDepthUseSpecTranslation = true;
+    tl::optional<bool> disparityToDepthUseSpecTranslation = tl::nullopt;
 
     /**
      * Obtain rectification matrices using spec translation (design data) or from calibration in calculations.
      * Suitable for debugging.
      * Default: false
      */
-    bool rectificationUseSpecTranslation = false;
+    tl::optional<bool> rectificationUseSpecTranslation = tl::nullopt;
 
     /**
      * Use baseline information for depth alignment from specs (design data) or from calibration.
      * Suitable for debugging.
      * Default: true
      */
-    bool depthAlignmentUseSpecTranslation = true;
+    tl::optional<bool> depthAlignmentUseSpecTranslation = tl::nullopt;
 
     /**
      * Free scaling parameter between 0 (when all the pixels in the undistorted image are valid)
