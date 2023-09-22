805b623396f8e32b31f5bb24b5da0873597fd41b
Martin Peterlin <martin.peterlin7@gmail.com>
2023-09-23 00:22:25 +0200

    Applied formatting

--- include/depthai-shared/common/EepromData.hpp
+++ include/depthai-shared/common/EepromData.hpp
@@ -19,7 +19,7 @@ namespace dai {
 struct EepromData {
     uint32_t version = 7;
     std::string productName, boardCustom, boardName, boardRev, boardConf, hardwareConf, deviceName;
-    std::string batchName; /// Deprecated, not used or stored
+    std::string batchName;  /// Deprecated, not used or stored
     uint64_t batchTime{0};
     uint32_t boardOptions{0};
     std::unordered_map<CameraBoardSocket, CameraInfo> cameraData;
