af95e17bf133b68f419d8961ddc18f643a41fdc7
asahtik <asahtik@gmail.com>
2023-10-16 13:58:30 +0200

    Added num attempts to sync properties

--- include/depthai-shared/properties/SyncProperties.hpp
+++ include/depthai-shared/properties/SyncProperties.hpp
@@ -12,6 +12,11 @@ struct SyncProperties : PropertiesSerializable<Properties, SyncProperties> {
      * The maximal interval the messages can be apart in milliseconds.
      */
     uint64_t syncIntervalMs = 10;
+
+    /**
+     * The number of syncing attempts before fail (num of replaced messages).
+     */
+    uint32_t syncAttempts = 50;
 };
 
 DEPTHAI_SERIALIZE_EXT(SyncProperties, syncIntervalMs);
