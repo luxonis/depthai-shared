9154b26b74608bb3a7bf461009afc3725370a105
asahtik <asahtik@gmail.com>
2023-11-15 09:06:29 +0100

    Removed demuxOnlyOnSuccessful opt

--- include/depthai-shared/properties/MessageDemuxProperties.hpp
+++ include/depthai-shared/properties/MessageDemuxProperties.hpp
@@ -8,9 +8,9 @@ namespace dai {
  * TODO
  */
 struct MessageDemuxProperties : PropertiesSerializable<Properties, MessageDemuxProperties> {
-    bool demuxOnlyOnSuccessful = false;
+    char dummy = 0;
 };
 
-DEPTHAI_SERIALIZE_EXT(MessageDemuxProperties, demuxOnlyOnSuccessful);
+DEPTHAI_SERIALIZE_EXT(MessageDemuxProperties, dummy);
 
 }  // namespace dai
