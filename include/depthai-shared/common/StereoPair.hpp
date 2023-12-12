91bd47af78db4639d55081f7afc998c6e1bf45fb
zrezke <jeretina.filip@gmail.com>
2023-12-12 16:51:27 +0100

    Added StereoPair.

---
+++ include/depthai-shared/common/StereoPair.hpp
@@ -0,0 +1,20 @@
+
+
+#pragma once
+
+#include "depthai-shared/common/CameraBoardSocket.hpp"
+namespace dai {
+/**
+ * Describes which camera sockets can be used for stereo and their baseline.
+ *
+ */
+struct StereoPair {
+    CameraBoardSocket left;
+    CameraBoardSocket right;
+    /**
+     * Baseline in centimeters.
+     */
+    float baseline = -1;
+    DEPTHAI_SERIALIZE(StereoPair, left, right, baseline);
+};
+}  // namespace dai
