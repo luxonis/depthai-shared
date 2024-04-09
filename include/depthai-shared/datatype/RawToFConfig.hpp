f77ed07190d60902728433a0d4afc93759720506
SzabolcsGergely <szabi@luxonis.com>
2024-04-09 22:26:30 +0300

    Update tof config

--- include/depthai-shared/datatype/RawToFConfig.hpp
+++ include/depthai-shared/datatype/RawToFConfig.hpp
@@ -13,46 +13,15 @@ namespace dai {
 /// RawToFConfig configuration structure
 struct RawToFConfig : public RawBuffer {
     /**
-     * DepthParams configuration structure.
+     * Set kernel size for depth median filtering, or disable
      */
-    struct DepthParams {
-        /**
-         * Enable averaging between phases with same modulation frequency(e.g. for ToF cameras with phase shuffle).
-         * The depth frame rate will be half if this is enabled
-         */
-        bool avgPhaseShuffle = false;
-
-        /**
-         * Perform depth calculation only for pixels with amplitude greater than provided value
-         */
-        float minimumAmplitude = 5.0;
-
-        /**
-         * Frequency modulation frames used for depth calculation. If the ToF sensor supports multiple modulation frequencies,
-         * all will be used for depth calculation.
-         */
-        enum class TypeFMod : std::int32_t { F_MOD_ALL, F_MOD_MIN, F_MOD_MAX };
-
-        TypeFMod freqModUsed = TypeFMod::F_MOD_MIN;
-
-        /**
-         * Set kernel size for depth median filtering, or disable
-         */
-        MedianFilter median = MedianFilter::KERNEL_5x5;
-
-        DEPTHAI_SERIALIZE(DepthParams, avgPhaseShuffle, minimumAmplitude, freqModUsed, median);
-    };
-
-    /**
-     * DepthParams configuration.
-     * Used for configuring the ToF.
-     */
-    DepthParams depthParams;
+    MedianFilter median = MedianFilter::MEDIAN_OFF;
 
     tl::optional<bool> enableFPPNCorrection;
     tl::optional<bool> enableOpticalCorrection;
     tl::optional<bool> enableTemperatureCorrection;
     tl::optional<bool> enableWiggleCorrection;
+    tl::optional<bool> enablePhaseUnwrapping;
 
     void serialize(std::vector<std::uint8_t>& metadata, DatatypeEnum& datatype) const override {
         metadata = utility::serialize(*this);
@@ -63,7 +32,8 @@ struct RawToFConfig : public RawBuffer {
         return DatatypeEnum::ToFConfig;
     }
 
-    DEPTHAI_SERIALIZE(RawToFConfig, depthParams, enableFPPNCorrection, enableOpticalCorrection, enableTemperatureCorrection, enableWiggleCorrection);
+    DEPTHAI_SERIALIZE(
+        RawToFConfig, median, enableFPPNCorrection, enableOpticalCorrection, enableTemperatureCorrection, enableWiggleCorrection, enablePhaseUnwrapping);
 };
 
 }  // namespace dai
