7effb90e5a04722d8657117609f1d091cf4b1b5c
SzabolcsGergely <szabi@luxonis.com>
2024-05-10 17:17:57 +0300

    Remove unused configs

--- include/depthai-shared/properties/DepthAlignProperties.hpp
+++ include/depthai-shared/properties/DepthAlignProperties.hpp
@@ -3,25 +3,20 @@
 #include <vector>
 
 #include "depthai-shared/common/optional.hpp"
-#include "depthai-shared/properties/Properties.hpp"
 #include "depthai-shared/datatype/RawDepthAlignConfig.hpp"
-#include "depthai-shared/common/CameraBoardSocket.hpp"
+#include "depthai-shared/properties/Properties.hpp"
 
 namespace dai {
 
 /**
- * Specify properties for DepthAlign 
+ * Specify properties for DepthAlign
  */
-struct DepthAlignProperties: PropertiesSerializable<Properties, DepthAlignProperties> {
+struct DepthAlignProperties : PropertiesSerializable<Properties, DepthAlignProperties> {
     RawDepthAlignConfig initialConfig;
-    CameraBoardSocket alignFrom = CameraBoardSocket::AUTO;
-    CameraBoardSocket alignTo = CameraBoardSocket::AUTO;
-    int alignWidth = 1280;
-    int alignHeight = 720;
-
-    bool downscaleSource = true;
+    int alignWidth = 0;
+    int alignHeight = 0;
 };
 
-DEPTHAI_SERIALIZE_EXT(DepthAlignProperties, initialConfig, alignFrom, alignTo, alignWidth, alignHeight, downscaleSource);
+DEPTHAI_SERIALIZE_EXT(DepthAlignProperties, initialConfig, alignWidth, alignHeight);
 
 }  // namespace dai
