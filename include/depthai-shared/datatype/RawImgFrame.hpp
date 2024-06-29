160f8b64737d55c5778b682c4b1288ab43eb08b3
alex-luxonis <alex@luxonis.com>
2024-06-30 01:12:51 +0300

    ImgFrame CameraSettings: add sensorTemperature, auxTemperature

--- include/depthai-shared/datatype/RawImgFrame.hpp
+++ include/depthai-shared/datatype/RawImgFrame.hpp
@@ -169,8 +169,10 @@ struct RawImgFrame : public RawBuffer {
         int32_t lensPosition;
         int32_t wbColorTemp;
         float lensPositionRaw;
+        float sensorTemperature;
+        float auxTemperature;
 
-        DEPTHAI_SERIALIZE(CameraSettings, exposureTimeUs, sensitivityIso, lensPosition, wbColorTemp, lensPositionRaw);
+        DEPTHAI_SERIALIZE(CameraSettings, exposureTimeUs, sensitivityIso, lensPosition, wbColorTemp, lensPositionRaw, sensorTemperature, auxTemperature);
     };
 
     Specs fb = {};
