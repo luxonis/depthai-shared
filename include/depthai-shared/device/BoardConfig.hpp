2d1b2066bea99ddb98c48b76218a62b0d5d65cd4
Serafadam <adam.serafin@luxonis.com>
2025-01-23 14:55:30 +0100

    add default imu extrinsics field

--- include/depthai-shared/device/BoardConfig.hpp
+++ include/depthai-shared/device/BoardConfig.hpp
@@ -8,6 +8,7 @@
 #include "depthai-shared/common/CameraBoardSocket.hpp"
 #include "depthai-shared/common/CameraImageOrientation.hpp"
 #include "depthai-shared/common/CameraSensorType.hpp"
+#include "depthai-shared/common/Extrinsics.hpp"
 #include "depthai-shared/common/UsbSpeed.hpp"
 #include "depthai-shared/common/optional.hpp"
 #include "depthai-shared/datatype/RawImgFrame.hpp"
@@ -152,6 +153,7 @@ struct BoardConfig {
         UVC() : UVC(1920, 1080) {}
     };
     tl::optional<UVC> uvc;
+    std::unordered_map<std::string, Extrinsics> defaultImuExtr;
 };
 
 DEPTHAI_SERIALIZE_EXT(BoardConfig::USB, vid, pid, flashBootedVid, flashBootedPid, maxSpeed, productName, manufacturer);
@@ -179,6 +181,7 @@ DEPTHAI_SERIALIZE_EXT(BoardConfig,
                       nonExclusiveMode,
                       camera,
                       imu,
-                      uvc);
+                      uvc,
+                      defaultImuExtr);
 
 }  // namespace dai
