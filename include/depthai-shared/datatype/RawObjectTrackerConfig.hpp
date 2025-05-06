dc62bc3f11e9feb9b6f9b395b08b5029176ba531
SzabolcsGergely <szabi@luxonis.com>
2025-05-06 10:37:18 +0300

    Add ObjectTrackerConfig message

---
+++ include/depthai-shared/datatype/RawObjectTrackerConfig.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+
+#include "depthai-shared/datatype/DatatypeEnum.hpp"
+#include "depthai-shared/datatype/RawBuffer.hpp"
+#include "depthai-shared/utility/Serialization.hpp"
+
+namespace dai {
+
+/// RawObjectTrackerConfig configuration structure
+struct RawObjectTrackerConfig : public RawBuffer {
+
+    std::vector<int32_t> trackletIdsToRemove;
+
+    void serialize(std::vector<std::uint8_t>& metadata, DatatypeEnum& datatype) const override {
+        metadata = utility::serialize(*this);
+        datatype = DatatypeEnum::ObjectTrackerConfig;
+    };
+
+    DatatypeEnum getType() const override {
+        return DatatypeEnum::ObjectTrackerConfig;
+    }
+
+    DEPTHAI_SERIALIZE(RawObjectTrackerConfig, trackletIdsToRemove);
+};
+
+}  // namespace dai
